c970ca221985039c4487028cbef73c4cdb58e1bb
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-22 11:54:32 +0000

    Initial files

---
+++ cmake-build-debug/CMakeFiles/Projet_AED.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet_AED.dir/main.cpp.obj"
+  "Projet_AED.exe"
+  "Projet_AED.exe.manifest"
+  "Projet_AED.pdb"
+  "libProjet_AED.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Projet_AED.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
