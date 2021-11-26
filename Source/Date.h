2b970bd7edba4b6e696b6ed5ae16ef1dc30ca333
Carolina Brandão <up202004461@edu.fc.up.pt>
2021-11-26 15:54:44 +0000

    comentarios Date muito bonitinhos

--- Source/Date.h
+++ Source/Date.h
@@ -1,22 +1,66 @@
 #ifndef DATE_H
 #define DATE_H
 
+/**
+ * A class that represents a Date
+ */
 class Date {
-    public:
-        Date();
-        Date(int day, int month, int year);
-        int getDay();
-        int getMonth();
-        int getYear();
-        void setDay(int day);
-        void setMonth(int month);
-        void setYear(int year);
 
-
-private:
+    private:
         int day;
         int month;
         int year;
+
+    public:
+        /**
+        * A default constructor of class Date.
+        */
+        Date();
+
+        /**
+         * Main constructor
+         * @param day Date's day, a int passed by value
+         * @param month Date's month, a int passed by value
+         * @param year Date's year, a int passed by value
+         */
+        Date(int day, int month, int year);
+
+        /**
+         * Return Date's day
+         * @return a int, containing date's day
+         */
+        int getDay();
+
+        /**
+         * Return Date's month
+         * @return a int, containing date's month
+         */
+        int getMonth();
+
+        /**
+         * Return Date's year
+         * @return a int, containing date's year
+         */
+        int getYear();
+
+        /**
+         * Set Date day
+         * @param day Date's new day, a int passed by value
+         */
+        void setDay(int day);
+
+        /**
+         * Set Date month
+         * @param month Date's new month, a int passed by value
+         */
+        void setMonth(int month);
+
+        /**
+         * Set Date year
+         * @param year Date's new year, a int passed by value
+         */
+        void setYear(int year);
+
 }
 
 #endif //DATE_H
