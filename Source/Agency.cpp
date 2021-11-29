342252bdc1ebf0202af6991e79ecc0dc41d3b6b9
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-29 03:37:27 +0000

    Agency initial files

---
+++ Source/Agency.cpp
@@ -0,0 +1,6 @@
+#ifndef PROJECT_AED_AGENCY_CPP
+#define PROJET_AED_AGENCY_CPP
+
+#include "Agency.h"
+
+#endif // PROJECT_AED_AGENCY_CPP
---
+++ Source/Agency.h
@@ -0,0 +1,15 @@
+#ifndef PROJECT_AED_AGENCY_H
+#define PROJET_AED_AGENCY_H
+
+#include <string>
+#include <vector>
+#include "Plane.h"
+
+class Agency {
+    // nome, vector de aeroportos, ... mais
+};
+
+// função que retorna um aeroporto com base numa string origem
+// ... mais
+
+#endif // PROJECT_AED_AGENCY_H
