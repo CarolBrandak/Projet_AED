a1480a6b6ff0dbf7669ce83854f42735183f0f54
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-29 18:21:38 +0000

    Added service test -> with bug :(

--- Tests/ServiceTest.cpp
+++ Tests/ServiceTest.cpp
@@ -6,11 +6,23 @@ using testing::Eq;
 
 TEST(Service, gettersAndSetters) {
 
-
+    Employee e1 = Employee("Pedro", 20, 'M', "FEUP2021");
+    Employee e2 = Employee("Carlos", 20 , 'm', "ISCTE21");
+    Service s1 = Service("Cleaning", Date(29, 11, 2021, 19, 47), e1);
 
     // Getters
-
+    ASSERT_EQ("Cleaning", s1.getServiceType());
+    ASSERT_EQ(Date(29, 11, 2021, 19, 47), s1.getServiceDate());
+    ASSERT_EQ(e1, s1.getResponsible());
+    ASSERT_EQ("Pedro", s1.getResponsible().getName());
 
     // Setters
+    s1.setResponsible(e2);
+    s1.getResponsible().setName("Joana");
+    ASSERT_EQ(e2, s1.getResponsible());
+    ASSERT_EQ("Joana", s1.getResponsible().getName());
 
+    // Operator <<
+    cout << s1;
+    cout << s1.getResponsible();
 }
