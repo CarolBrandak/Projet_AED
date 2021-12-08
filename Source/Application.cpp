813d22636b2b09b177a7cebb4b06f70bd189e539
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-08 19:42:11 +0000

    Simple application

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -1,15 +1,8 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-
 #include "Company.h"
-
-using namespace std;
 
 void Application() {
 
     Company company = Company("AirED");
-    company.printData();
+    company.saveData();
 
 }
