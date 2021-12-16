0e8f8a9319f54a934ff134ca8ec14de339ecbf17
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-16 00:43:24 +0000

    Carts check

--- Source/Cart.cpp
+++ Source/Cart.cpp
@@ -1,9 +1,9 @@
+#ifndef PROJECT_AED_CART_CPP
+#define PROJECT_AED_CART_CPP
 
 #include "Cart.h"
 
-Cart::Cart() : STACK_SIZE(4), QUEUE_SIZE(3) {
-    this->transport = {};
-}
+Cart::Cart() {}
 
 void Cart::addLuggage(queue<Luggage *> treadmill) {
 
@@ -15,23 +15,21 @@ void Cart::addLuggage(queue<Luggage *> treadmill) {
         Luggage* currentLuggage = treadmill.front();
         treadmill.pop();
 
-        // Se tiver espaço na pilha, coloca na pilha
         if (currentStack.size() < STACK_SIZE) {
             currentStack.push(currentLuggage);
         } else {
-            // Se tiver ainda espaço na fila, coloca na fila e esvazia a stack para a próxima iteração
+
             if (currentQueue.size() < QUEUE_SIZE) {
                 currentQueue.push(currentStack);
                 while (!currentStack.empty()) currentStack.pop();
             } else {
-                // Senão, coloca no transporte o carrinho, esvazia a stack e a fila para a próxima iteração
+
                 transport.push_back(currentQueue);
                 while (!currentStack.empty()) currentStack.pop();
                 while (!currentQueue.empty()) currentQueue.pop();
             }
         }
     }
-    // No final, o restante que sobra também é adicionado
     currentQueue.push(currentStack);
     transport.push_back(currentQueue);
 }
@@ -49,4 +47,4 @@ void Cart::putLuggage(Flight* flight) {
     transport.clear();
 }
 
-#include "Cart.h"
+#endif // PROJECT_AED_CART_CPP
--- Source/Cart.h
+++ Source/Cart.h
@@ -1,22 +1,42 @@
-//
-// Created by Fábio Sá on 15/12/2021.
-//
+#ifndef PROJECT_AED_CART_H
+#define PROJECT_AED_CART_H
 
-#ifndef PROJET_AED_CART_H
-#define PROJET_AED_CART_H
+#define STACK_SIZE 4
+#define QUEUE_SIZE 3
+
 #include "Luggage.h"
 #include "Flight.h"
 
+/**
+ * A class to manage Luggage Cart
+ */
 class Cart {
 
-private:
-    const int STACK_SIZE; // n de malas em cada pilha
-    const int QUEUE_SIZE; // n de pilhas em cada carruagem
-    list<queue<stack<Luggage*>>> transport;
-public:
-    Cart();
-    void addLuggage(queue<Luggage *> treadmill);
-    void putLuggage(Flight* flight);
+    private:
+
+        /**
+         * @var transport - a list of queues of stacks that represent the main organization of current Cart
+         */
+        list<queue<stack<Luggage*>>> transport;
+
+    public:
+
+        /**
+         * A default constructor of the class. No arguments needed.
+         */
+        Cart();
+
+        /**
+         * A function that adds luggage in current cart
+         * @param treadmill - a queue based on Luggage pointer objects that contains all Luggage in current Order
+         */
+        void addLuggage(queue<Luggage *> treadmill);
+
+        /**
+         * A function that adds luggage in current flight
+         * @param flight - Flight object where the luggage will be dropped off
+         */
+        void putLuggage(Flight* flight);
 };
 
-#endif //PROJET_AED_CART_H
+#endif // PROJECT_AED_CART_H
